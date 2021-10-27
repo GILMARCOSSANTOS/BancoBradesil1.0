4908b0c04ea0161ba0909f5b94face493e1c2b27
Gilmarcos Santos <54557299+GILMARCOSSANTOS@users.noreply.github.com>
2021-10-27 19:49:00 -0300

    Commit Anterior ao uso do Shared Preference Store

--- app/src/main/java/com/example/bancobradesil10/ActivityConta.kt
+++ app/src/main/java/com/example/bancobradesil10/ActivityConta.kt
@@ -1,19 +1,33 @@
 package com.example.bancobradesil10
 
-import android.R.id
-import android.content.ClipData
+import android.app.ProgressDialog.show
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Xml
 import android.view.*
+import android.view.Gravity.*
 import android.widget.*
-import android.graphics.drawable.Drawable as Drawable1
-import android.R.id.button1
+import androidx.core.graphics.green
+import android.widget.PopupWindow
+import com.google.android.material.transition.MaterialSharedAxis
+import android.graphics.drawable.Drawable
+
+
+import android.widget.FrameLayout
+
+
+import android.view.LayoutInflater
+
+import android.R.layout
+import android.content.Context
+import androidx.core.widget.PopupWindowCompat.showAsDropDown
+import kotlin.math.absoluteValue
+
 
 class ActivityConta : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conta)
@@ -31,7 +45,7 @@ class ActivityConta : AppCompatActivity() {
         val botaoSair = findViewById<ImageView>(R.id.imageViewSairActivityContaId)
         val nomeCliente = findViewById<TextView>(R.id.textViewNomeClienteActivityContaId)
         val mensagemNomeActivityConta = intent.getStringExtra("chaveNomeConta")
-        val faleBia = findViewById<EditText>(R.id.editTextFaleBiaContaActivityId)
+        val faleBia = findViewById<AutoCompleteTextView>(R.id.autoCompleteFaleBiaContaActivityId)
 
         /**
          * Recebimento dos dados do nome da Conta:
@@ -49,49 +63,59 @@ class ActivityConta : AppCompatActivity() {
         )
 
         /**
-         * Invocar para usar a função Menu:
-         */
-        registerForContextMenu(faleBia)
-
-        /**
          * Criar Funções:
          */
         botaoSair.setOnClickListener { botaoSair() }
         imageViewVisivel.setOnClickListener { olhoVisivel() }
         imageViewNaoVisivel.setOnClickListener { olhoNaoVisivel() }
-        faleBia.setOnClickListener{ faleComBia()}
+        faleBia.setOnClickListener { faleComBia() }
 
     }
+
 
     /**
      * Função Menu de Opções FaleComBia():
      */
     private fun faleComBia() {
-        val faleBia = findViewById<EditText>(R.id.editTextFaleBiaContaActivityId)
-        showPopup(faleBia)
+
+        val faleBia = findViewById<AutoCompleteTextView>(R.id.autoCompleteFaleBiaContaActivityId)
+        val opcoesMenuBia = arrayOf<String>(
+            "Quero meu informe de rendimentos",
+            "Quero pagar uma conta",
+            "Preciso transferir dinheiro"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, opcoesMenuBia)
+        faleBia.setAdapter(adapter)
+
+        when (faleBia.text.toString()) {
+            "Quero meu informe de rendimentos" -> Toast.makeText(
+                this,
+                "Rendimentos",
+                Toast.LENGTH_SHORT
+            ).show()
+            "Quero pagar uma conta" -> Toast.makeText(this, "Pagar", Toast.LENGTH_SHORT).show()
+            "Preciso transferir dinheiro" -> Toast.makeText(this, "Teansferir", Toast.LENGTH_SHORT)
+                .show()
+        }
+        faleBia.showDropDown()
     }
-
-    private fun showPopup(view: View){
-        val popup = PopupMenu(this, view)
-        popup.inflate(R.menu.meu_menu_um)
-        popup.show()
-    }
-
-
-
-
 
     /**
      * Função olhoNaoVisivel():
      */
     private fun olhoNaoVisivel() {
         val textViewSaldo = findViewById<TextView>(R.id.textViewSaldoReaisContaActivityId)
-        val imageViewNaoVisivel = findViewById<ImageView>(R.id.imageViewNaoVisivelActivityContaId)
+        val imageViewNaoVisivel =
+            findViewById<ImageView>(R.id.imageViewNaoVisivelActivityContaId)
         val imageViewVisivel = findViewById<ImageView>(R.id.imageViewVisivelActivityContaId)
 
         if (imageViewNaoVisivel.isClickable) {
             textViewSaldo.background = getDrawable(R.drawable.botao_redondo03)
-            imageViewNaoVisivel.setColorFilter(R.color.ocultarOlhoVisivel, PorterDuff.Mode.CLEAR)
+            imageViewNaoVisivel.setColorFilter(
+                R.color.ocultarOlhoVisivel,
+                PorterDuff.Mode.CLEAR
+            )
             imageViewVisivel.setColorFilter(R.color.white01, PorterDuff.Mode.DST)
         }
     }
@@ -101,7 +125,8 @@ class ActivityConta : AppCompatActivity() {
      */
     private fun olhoVisivel() {
         val textViewSaldo = findViewById<TextView>(R.id.textViewSaldoReaisContaActivityId)
-        val imageViewNaoVisivel = findViewById<ImageView>(R.id.imageViewNaoVisivelActivityContaId)
+        val imageViewNaoVisivel =
+            findViewById<ImageView>(R.id.imageViewNaoVisivelActivityContaId)
         val imageViewVisivel = findViewById<ImageView>(R.id.imageViewVisivelActivityContaId)
 
         if (imageViewVisivel.isClickable) {
@@ -118,4 +143,10 @@ class ActivityConta : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
 }
+
+
+
+
+
